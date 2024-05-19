1d1416d10ded2e193a9b8c8d6c8893eef9e13268
Ion Ciubaciuc <ion.ciubaciuc@hotmail.com>
2024-05-19 15:15:05 +0200

    Assets location

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
 import { Gallery } from './gallery';
-import pictureOne from './assets/picture_1.webp';
-import pictureOneThumbnail from './assets/picture_1_thumbnail.webp';
-import pictureTwo from './assets/picture_2.webp';
-import pictureTwoThumbnail from './assets/picture_2_thumbnail.webp';
-import pictureThree from './assets/picture_3.webp';
-import pictureThreeThumbnail from './assets/picture_3_thumbnail.webp';
-import pictureFour from './assets/picture_4.webp';
-import pictureFourThumbnail from './assets/picture_4_thumbnail.webp';
-import pictureFive from './assets/picture_5.webp';
-import pictureFiveThumbnail from './assets/picture_5_thumbnail.webp';
 
 const images = [
     {
-        slide: { src: pictureOne },
-        thumbnail: { src: pictureOneThumbnail },
+        slide: { src: '/assets/picture_1.webp' },
+        thumbnail: { src: '/assets/picture_1_thumbnail.webp' },
     },
     {
-        slide: { src: pictureTwo },
-        thumbnail: { src: pictureTwoThumbnail },
+        slide: { src: '/assets/picture_2.webp' },
+        thumbnail: { src: '/assets/picture_2_thumbnail.webp' },
     },
     {
-        slide: { src: pictureThree },
-        thumbnail: { src: pictureThreeThumbnail },
+        slide: { src: '/assets/picture_3.webp' },
+        thumbnail: { src: '/assets/picture_3_thumbnail.webp' },
     },
     {
-        slide: { src: pictureFour },
-        thumbnail: { src: pictureFourThumbnail },
+        slide: { src: '/assets/picture_4.webp' },
+        thumbnail: { src: '/assets/picture_4_thumbnail.webp' },
     },
     {
-        slide: { src: pictureFive },
-        thumbnail: { src: pictureFiveThumbnail },
+        slide: { src: '/assets/picture_5.webp' },
+        thumbnail: { src: '/assets/picture_5_thumbnail.webp' },
     },
 ];
 
